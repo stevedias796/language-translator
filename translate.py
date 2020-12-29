902157c02a385a4c4d8cdd8be155132815e1347e
stevedias796 <55200261+stevedias796@users.noreply.github.com>
2020-12-30 01:37:29 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -7,6 +7,24 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/textTranslate')
+def text_translate():
+    return render_template('textTrans.html', result="")
+
+
+@app.route('/text_result', methods=['POST', 'GET'])
+def text_result():
+    if request.method == 'POST':
+        input_text = []
+        input_text.append(request.form['result'])
+        print(input_text)
+        trans = google_translator()
+
+        text = trans.translate(text=input_text[0], lang_tgt='hi', lang_src='en')
+        print(text)
+        return render_template("textTrans.html", result=text)
+
+
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
